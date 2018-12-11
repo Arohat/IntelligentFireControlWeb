958c18c9db2582b16cb8e51aebe6ab8aa4040e1f
hongjinfeng <hongjinfeng@foisonagel.com>
2018-12-11 11:31:05 +0800

    UI样式引用

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="login_page">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
-  <el-form-item prop="name">
-    <el-input v-model="ruleForm.name"></el-input>
-  </el-form-item>
-  <el-form-item prop="password">
-    <el-input v-model="ruleForm.password"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
-  </el-form-item>
-</el-form>
+    <div class="logo"></div>
+        <div class="login-panel">
+        <h1>智慧消防系统</h1>
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="ruleForm">
+                <el-form-item prop="name">
+                    <el-input v-model="ruleForm.name">
+                    <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input v-model="ruleForm.password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 <script>
@@ -59,6 +65,30 @@
     -moz-background-size: cover;
     -o-background-size: cover;
     background-attachment: fixed;
+}
+.logo {
+    position: fixed;
+    top: 3.4%;
+    left: 5%;
+    background: url(../assets/img/loginlogo.png) no-repeat;
+    width: 170px;
+    height: 55px;
+}
+.login-panel {
+    width: 438px;
+    height: 424px;
+    overflow: hidden;
+    background-color: #fff;
+    margin: 0;
+    padding: 20px 30px;
+    position: fixed;
+    top: 17%;
+    right: 8.6%;
+}
+h1 {
+    font-size: 24px;
+    color: #333;
+    text-align: center;
 }
 </style>
 
