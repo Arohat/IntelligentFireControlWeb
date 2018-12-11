52d152e0304496911d64f6f2cd15b81aaf49b200
hongjinfeng <hongjinfeng@foisonagel.com>
2018-12-11 10:55:44 +0800

    添加登陆页

---
+++ src/page/login.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="login_page">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+  <el-form-item prop="name">
+    <el-input v-model="ruleForm.name"></el-input>
+  </el-form-item>
+  <el-form-item prop="password">
+    <el-input v-model="ruleForm.password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+  </el-form-item>
+</el-form>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          name: '',
+          password: ''
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请选择活动区域', trigger: 'change' }
+          ]
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+<style>
+.login_page{
+    width: 100%;
+    height: 100%;
+    background: url('../assets/img/loginbg.png');
+    background-size: cover;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-attachment: fixed;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,19 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+const login = r => require.ensure( [], () => r( require( '@/page/login' ) ), 'login' );
+
 export default new Router({
   routes: [
+  {
+        path: '/',
+        component: login,
+        meta:{
+            isHadLogin:true,
+        }
+    },
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }
