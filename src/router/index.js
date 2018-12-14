e3c0b369b4f3df7f8a7bffd15354714c400bf12a
Yxl1 <329542513@qq.com>
2018-12-14 09:26:13 +0800

    111

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import manage from '@/page/manage'
+import manage from '@/page/index'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -9,6 +9,8 @@ const login = r => require.ensure([], () => r(require('@/page/login')), 'login')
 const userManagement = r => require.ensure([], () => r(require('@/components/userManagement')), 'userManagement');
 const informationAdded = r => require.ensure([], () => r(require('@/components/informationAdded')), 'informationAdded');
 const department = r => require.ensure([], () => r(require('@/components/department')), 'department');
+const systemPermissions = r => require.ensure([], () => r(require('@/page/systemManagement/basicManagement/systemPermissions')), 'systemPermissions');
+const systemPermissionsAdd = r => require.ensure([], () => r(require('@/page/systemManagement/basicManagement/systemPermissionsAdd')), 'systemPermissionsAdd');
 
 export default new Router({
   routes: [
@@ -34,7 +36,17 @@ export default new Router({
         path: '/userManagement',
         component: userManagement,
         meta: [{title: '用户管理', path: '/userManagement'}, {title: '用户管理', path: '/userManagement'}],
-      }, {
+      },
+      {
+        path: '/systemManagement/basicManagement/systemPermissions',
+        component: systemPermissions,
+        meta: [{title: '系统权限', path: '/systemManagement/basicManagement/systemPermissions'}, {title: '系统权限', path: '/systemManagement/basicManagement/systemPermissions'}],
+      }, 
+      {
+        path: '/systemManagement/basicManagement/systemPermissionsAdd',
+        component: systemPermissionsAdd,
+        meta: [{title: '添加系统权限', path: '/systemManagement/basicManagement/systemPermissionsAdd'}, {title: '添加系统权限', path: '/systemManagement/basicManagement/systemPermissionsAdd'}],
+      },{
           path: '/informationAdded',
           component: informationAdded,
           meta: [{title: '用户管理', path: '/informationAdded'}, {title: '用户管理', path: '/informationAdded'}],
