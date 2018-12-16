56289be054a93d7828893f09db950358bace8152
zc1428580764 <1428580764@qq.com>
2018-12-16 16:47:49 +0800

    代码回滚

--- src/page/index.vue
+++ src/page/index.vue
@@ -3,51 +3,33 @@
     <div class="layout-header">
       <Header></Header>
     </div>
-    <div class="layout-container">
+    <!-- <div class="layout-container">
       <div class="layout-left">
-        <SideBar v-show="showSideBar"></SideBar>
-      </div>
-      <div class="layout-right">
+        <SideBar></SideBar>
+      </div> -->
+     <!-- <div class="layout-right">
         <Container>
           <router-view></router-view>
         </Container>
-      </div>
+      </div> -->
+			<div>这是首页。。。。。。。。。。。。。。。。。。。。。。。。</div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "../common/Header";
-import SideBar from "../common/SideBar";
-import Container from "../common/Container";
-import Bus from '../Bus.js'
-
+/* import SideBar from "../common/SideBar";
+import Container from "../common/Container"; */
 export default {
   name: "CloudHome",
   data() {
-    return {
-        showSideBar:true
-    };
+    return {};
   },
-    mounted: function () {
-        Bus.$on('on', (msg) => {
-            console.log(msg)
-            if(msg.navigateUrl!='index'){
-                this.showSideBar = true
-            }else {
-                this.showSideBar = false
-            }
-        });
-      if(window.location.hash!='#/index'){
-          this.showSideBar = true
-      }else {
-          this.showSideBar = false
-      }
-    },
   components: {
-    Header ,
+    Header/* ,
     SideBar,
-    Container
+    Container */
   }
 };
 </script>
