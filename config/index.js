5a4b9eb551868b1e066b65991f01e62cdb0a4eb2
gulezhidian <842693323@qq.com>
2018-12-16 16:58:28 +0800

    配置修改

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:8081/ifc',//后端接口地址
+        target: 'http://localhost:8081/ifc',//后端接口地址
         changeOrigin: true,//是否允许跨越
         pathRewrite: {
           '^/api': '/api',//重写,
