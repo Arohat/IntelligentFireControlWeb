399781a2c50ab89e32e212d405b0c9f8b466022e
Yxl1 <329542513@qq.com>
2018-12-17 09:13:13 +0800

    用户权限

--- src/page/systemManagement/basicManagement/systemPermissionsAdd.vue
+++ src/page/systemManagement/basicManagement/systemPermissionsAdd.vue
@@ -1,114 +1,150 @@
 <template>
-    <el-form class="container-form" ref="form" :model="form" label-width="80px">
-        <el-form-item label="所属模块">
-            <el-select v-model="form.region" placeholder="请选择活动区域">
-                <el-option label="请选择" value=""></el-option>
-                <el-option label="天津区域" value="shanghai"></el-option>
-                <el-option label="上海区域" value="beijing"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="所属项目">
-            <el-select v-model="form.project" placeholder="请选择活动区域">
-                <el-option label="请选择" value=""></el-option>
-                <el-option label="碧桂园" value="shanghai"></el-option>
-                <el-option label="万科新城" value="beijing"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="合同编号">
-            <el-input v-model="form.number"></el-input>
-        </el-form-item>
-        <el-form-item label="合同名称">
-            <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="维保公司">
-            <el-select v-model="form.Maintenance" placeholder="请选择活动区域">
-                <el-option label="请选择" value=""></el-option>
-                <el-option label="测试公司1" value="shanghai"></el-option>
-                <el-option label="测试公司2" value="beijing"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="主联系人">
-            <el-input v-model="form.Contact"></el-input>
-        </el-form-item>
-        <el-form-item label="联系电话">
-            <el-input v-model="form.phone"></el-input>
-        </el-form-item>
-        <el-form-item label="开始日期">
-            <el-date-picker type="date" placeholder="开始日期" v-model="form.startDate" style="width: 100%;"></el-date-picker>
-        </el-form-item>
-        <el-form-item label="结束日期">
-            <el-time-picker type="fixed-time" placeholder="结束日期" v-model="form.endDate" style="width: 100%;"></el-time-picker>
-        </el-form-item>
-        <el-form-item label="合同状态">
-            <el-select v-model="form.status" placeholder="请选择活动区域">
-                <el-option label="请选择" value=""></el-option>
-                <el-option label="有效" value="有效"></el-option>
-                <el-option label="失效" value="失效"></el-option>
-                <el-option label="作废" value="作废"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="备注信息">
-            <el-input type="textarea" v-model="form.desc"></el-input>
-        </el-form-item>
-        <el-form-item label="备注信息">
-            <el-upload
-                    class="upload-demo"
-                    ref="upload"
-                    action="https://jsonplaceholder.typicode.com/posts/"
-                    :on-remove="handleRemove"
-                    :file-list="form.fileList"
-                    :multiple="false"
-                    :on-change="handlebeforeupload"
-                    :auto-upload="false">
-                <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
-                <!--<el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>-->
-                <!--<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
-            </el-upload>
-        </el-form-item>
-        <el-form-item>
-            <el-button type="success" round @click="onSubmit">保存</el-button>
-            <el-button type="info" round>返回</el-button>
-        </el-form-item>
-    </el-form>
+	<div>
+		<el-breadcrumb separator-class="el-icon-arrow-right">
+	      <el-breadcrumb-item :to="{ path: '/manage' }">基础管理</el-breadcrumb-item>
+	      <el-breadcrumb-item>系统权限</el-breadcrumb-item>
+	      <el-breadcrumb-item>编辑</el-breadcrumb-item>
+	    </el-breadcrumb>
+		<el-form class="container-form" :model="form" :rules="rules" ref="form" label-width="200px">
+	        <el-form-item label="所属模块" prop="displayName">
+	            <el-select v-model="form.modelName" placeholder="请选择所属模块">
+	                <el-option
+				      v-for="item in modelNameoptions"
+				      :key="item"
+				      :label="item"
+				      :value="item">
+				    </el-option>
+	            </el-select>
+	        </el-form-item>
+	        
+	        <el-form-item label="系统权限名称" prop="powerName">
+	            <el-input v-model="form.powerName" placeholder="请输入系统权限名称"></el-input>
+	        </el-form-item>
+	        <el-form-item label="系统权限值" prop="displayName">
+	            <el-input v-model="form.displayName" placeholder="请输入系统权限值"></el-input>
+	        </el-form-item>
+	 
+	        <el-form-item label="备注信息">
+	            <el-input type="textarea" v-model="form.remark"></el-input>
+	        </el-form-item>
+	       
+	        <el-form-item>
+	            <el-button type="success" round @click="onSubmit('form')">保存</el-button>
+	            <el-button type="info" @click="goBack('form')" round>返回</el-button>
+	        </el-form-item>
+	    </el-form>
+	</div>
+    
 </template>
 
 <script>
+	import api from '@/api/api';
     export default {
         data() {
             return {
+            	paramPageNum:'',
+            	modelNameoptions:[],
                 form: {
-                    number: '',
-                    Maintenance:'',
-                    Contact:'',
-                    phone:'',
-                    name: '',
-                    status: '',
-                    region: '',
-                    project:'',
-                    startDate: '',
-                    endDate: '',
-                    desc: '',
-                    fileList: []
-                }
+                    modelName: '',
+                    powerName:'',
+                    displayName:'',
+                    remark:'',
+                },
+                rules: {
+			          modelName: [
+			            { required: true, message: '请选择消息类型', trigger: 'change' }
+			          ],
+			          powerName: [
+			            { required: true, message: '权限名称不能为空', trigger: 'blur' }
+			          ],
+			          displayName: [
+			            { required: true, message: '权限展示名称不能为空', trigger: 'blur' }
+			          ]
+			    }
             }
         },
+        created:function() {
+        	this.paramPageNum = this.$route.query.parampageNum
+        	console.log(this.paramPageNum)
+        	this.$http2.post('/basic/modelPower/getModelName', {})
+		          .then(data => {
+		          	if(data.code==0){
+		          		this.modelNameoptions = data.data
+		          	}
+		          }).catch(()=>{
+		          console.log("失败");
+		        });
+        },
         methods: {
-            onSubmit() {
-                let query = this.form;
-                console.log(query);
-                this.$http2.post("/sys/informationAdded", query)
-                    .then(data => {
-                        console.log("成功");
-                    }).catch(()=>{
-                    console.log("失败");
-                });
+        	getModelName(){
+        		this.$http2.post(api.getModelName, {})
+		          .then(data => {
+		          	if(data.code==0){
+		          		this.modelNameoptions = data.data
+		          	}
+		          }).catch(()=>{
+		          console.log("失败");
+		        });
+        	},
+            onSubmit(formName) {
+            	var param = this.form
+                this.$refs[formName].validate((valid) => {
+		          if (valid) {
+		            this.$http2.post("/basic/modelPower/saveModelPower", param)
+			          .then(data => {
+			          	if(data.code==0){
+			          		this.successOpen()
+			          	}else{
+			          		this.faOpen()
+			          	}
+			          }).catch(()=>{
+			          	console.log("失败");
+			        });
+		          } else {
+		            return false;
+		          }
+		        });
             },
-            handlebeforeupload(file,fileList){
-                this.form.fileList = fileList
+            goBack(formName){
+            	this.$refs[formName].resetFields();
+            	this.$router.push({
+				    path: '/systemManagement/basicManagement/systemPermissions',
+				    query: {
+				    	parampageNum:this.paramPageNum
+				    }
+				})
             },
-            handleRemove(file, fileList) {
-                this.form.fileList = fileList
+            modelNameChange(val){
+            	console.log(this.form.modelName)
             },
+            successOpen() {
+		        this.$alert('操作成功', '信息', {
+		          confirmButtonText: '确定',
+		          callback: action => {
+//		            this.$message({
+//		              type: 'success',
+//		              message: `保存成功`
+//		            });
+		            this.$router.push({
+					    path: '/systemManagement/basicManagement/systemPermissions',
+					    query: {
+					    	parampageNum:1
+					    }
+					})
+		          }
+		        });
+		     },
+		     faOpen() {
+		        this.$alert('操作失败', '信息', {
+		          confirmButtonText: '确定',
+		          callback: action => {
+//		            this.$message({
+//		              type: 'danger',
+//		              message: `保存失败`
+//		            });
+		          }
+		        });
+		     }
         }
     }
 </script>
